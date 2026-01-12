2d16c790695a01f6adde680bd428935e6436230d
Park, Daekyeong <pdk2037@gmail.com>
2026-01-13 00:18:12 +0900

    init: springboot 프로젝트 생성

---
+++ src/main/java/gdg/beforeonebite/BeforeOneBiteApplication.java
@@ -0,0 +1,13 @@
+package gdg.beforeonebite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeforeOneBiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeforeOneBiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gdg/beforeonebite/BeforeOneBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package gdg.beforeonebite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeforeOneBiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
