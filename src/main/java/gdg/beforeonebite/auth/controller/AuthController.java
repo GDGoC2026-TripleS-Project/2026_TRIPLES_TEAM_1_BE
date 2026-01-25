3d0ae47cb4127f088b7903fccbab0483c106f7e9
0xwb7 <woobin200111@gmail.com>
2026-01-26 02:53:57 +0900

    refactor: 와일드카드 import 수정 (#2)

--- src/main/java/gdg/beforeonebite/auth/controller/AuthController.java
+++ src/main/java/gdg/beforeonebite/auth/controller/AuthController.java
@@ -9,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
